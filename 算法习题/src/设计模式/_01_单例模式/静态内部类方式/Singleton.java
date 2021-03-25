1f6d0ca5de3733109f2e014b0c03a0066e84257e
xuechun-fan <52503118+xuechun-fan@users.noreply.github.com>
2021-03-25 10:33:09 +0800

    update单例模式的代码

---
+++ 算法习题/src/设计模式/_01_单例模式/静态内部类方式/Singleton.java
@@ -0,0 +1,14 @@
+package 设计模式._01_单例模式.静态内部类方式;
+
+/**
+ * 静态内部类方式，多线程安全
+ */
+class Singleton{
+    private Singleton(){}
+    private static class InstanceHolder{
+        private static final Singleton instance = new Singleton();
+    }
+    public static Singleton getInstance(){
+        return InstanceHolder.instance;
+    }
+}
