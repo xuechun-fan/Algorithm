7be1db4d508e4bdae2f0c3b908ee397d5c3ea7f3
xuechun-fan <52503118+xuechun-fan@users.noreply.github.com>
2021-04-08 22:03:25 +0800

    更新几道题，

---
+++ 算法习题/src/剑指Offer习题/面试题43_1到n整数中1出现的次数.java
@@ -0,0 +1,13 @@
+package 剑指Offer习题;
+
+/**
+ * Description：输入一个整数 n ，求1～n这n个整数的十进制表示中1出现的次数。
+ *
+ * 例如，输入12，1～12这些整数中包含1 的数字有1、10、11和12，1一共出现了5次。
+ */
+public class 面试题43_1到n整数中1出现的次数 {
+    public static int countDigitOne(int n) {
+        int res = 0;
+
+    }
+}
